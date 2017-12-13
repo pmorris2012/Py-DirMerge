c9251298d3688b3171a281210e7e7a0e13a693a0
Paul Morris <pmorris2012@fau.edu>
2017-12-13 11:39:44 -0500

    more robust cmd line args

--- dmerge.py
+++ dmerge.py
@@ -20,9 +20,9 @@ def get_flist(path):
             yield os.path.join(root, f)
 
 def get_unique(dir1, dir2, dir1_files, dir2_files):
-    dir1_set = set([x.replace(dir1, '') for x in dir1_files])
-    dir2_set = set([x.replace(dir2, '') for x in dir2_files])
-    return [dir1 + x for x in (dir1_set - dir2_set)], [dir2 + x for x in (dir2_set - dir1_set)]
+    dir1_set = set([os.path.normpath(x.replace(dir1, '')) for x in dir1_files])
+    dir2_set = set([os.path.normpath(x.replace(dir2, '')) for x in dir2_files])
+    return [os.path.join(dir1, x) for x in (dir1_set - dir2_set)], [os.path.join(dir2, x) for x in (dir2_set - dir1_set)]
 
 #logic from https://gist.github.com/jacobtomlinson/9031697
 def op_del_empty_dirs(path):
@@ -58,6 +58,8 @@ parser.add_argument('dir2', help='absolute path of the second directory to compa
 parser.add_argument('ops', type=str, choices=ops, nargs='+', help='Specify any number of operations to perform from the list:\n del-1-2: delete all files in 2 which are not in 1\n del-2-1: delete all files in 1 which are not in 2\n merge-1-2: move files in 1 to 2, without replacement\n merge-2-1: move files in 2 to 1, without replacement\n copy-1-2: move files in 1 to 2, with replacement\n copy-2-1: move files in 2 to 1, with replacement\n compare: display dir info')
 
 args = parser.parse_args()
+args.dir1 = os.path.join(args.dir1, '')
+args.dir2 = os.path.join(args.dir2, '')
 
 for op in args.ops:
     dir1_files = list(get_flist(args.dir1))
@@ -90,4 +92,3 @@ for op in args.ops:
         op_copy(dir2_files, args.dir2, args.dir1)
     elif op == 'compare':
         pass
-        print(dir1_uniq[0], dir2_uniq[0])
